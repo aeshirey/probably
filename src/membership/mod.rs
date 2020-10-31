ea082740e4e5add516df72200bfb7dfa50906d8f
Adam Shirey <adam.shirey@gmail.com>
2020-10-31 10:18:56 -0700

    Cuckoo filter

--- src/membership/mod.rs
+++ src/membership/mod.rs
@@ -1,3 +1,6 @@
 pub(crate) mod bloom_filter;
 
 pub use bloom_filter::{Bloom, BloomBuilder, BloomError};
+
+pub(crate) mod cuckoo;
+pub use cuckoo::{CuckooError, CuckooFilter, ExportedCuckooFilter};
